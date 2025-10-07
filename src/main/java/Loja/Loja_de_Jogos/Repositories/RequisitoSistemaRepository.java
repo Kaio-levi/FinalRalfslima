1b339051f3728d3fab5bc6d4ec6e8131fd16ad4c
Lksmv <lks.migueel@gmail.com>
2025-10-07 20:13:31 -0300

    del

--- src/main/java/Loja/Loja_de_Jogos/Repositories/RequisitoSistemaRepository.java
+++
@@ -1,7 +0,0 @@
-package Loja.Loja_de_Jogos.Repositories;
-
-import Loja.Loja_de_Jogos.Models.RequisitoSistema;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RequisitoSistemaRepository extends JpaRepository<RequisitoSistema, Long> {
-}
